bcd9c8a6be8185cc95091eeb422370c0a4520706
kaulgudneha <63013040+kaulgudneha@users.noreply.github.com>
2020-06-01 19:37:09 +0530

    Add files via upload

---
+++ prog13student.py
@@ -0,0 +1,11 @@
+from collections import Counter
+classes = (
+    ('V', 1),
+    ('VI', 1),
+    ('V', 2),
+    ('VI', 2),
+    ('VI', 3),
+    ('VII', 1),
+)
+students = Counter(class_name for class_name, no_students in classes)
+print(students)
